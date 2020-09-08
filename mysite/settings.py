ba33de4e2ed7e62a4be5a75d4af6c35b8a3149d8
SubAkBa <circle5926@gmail.com>
2020-09-08 16:04:06 +0900

    Chapter 5-2 - Class based View & Root Page

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'polls.apps.PollsConfig',
-    'books.apps.BooksConfig',
+    # 'books.apps.BooksConfig',
+    'books',
 ]
 
 MIDDLEWARE = [
@@ -116,3 +117,28 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_logger': False,
+    'formatters': {
+        'verbose': {
+            'format': "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt': "%d/%b/%Y %H:%M:%S"
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, 'logs', 'mysite.log'),
+            'formatter': 'verbose'
+        },
+    },
+    'loggers': {
+        'polls': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+        },
+    },
+}
--- polls/views.py
+++ polls/views.py
@@ -1,23 +1,49 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.views.generic import ListView, DetailView
+
 from polls.models import Question, Choice
+import logging
+
+logger = logging.getLogger(__name__)
 
 
+# -- Class-based GenericView
 # Create your views here.
-def index(request):
-    latest_question_list = Question.objects.all().order_by('-pub_date')[:5]
+# def index(request):
+#     latest_question_list = Question.objects.all().order_by('-pub_date')[:5]
+#
+#     return render(request, 'polls/index.html', {'latest_question_list': latest_question_list})
+class IndexView(ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-    return render(request, 'polls/index.html', {'latest_question_list': latest_question_list})
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#
+#     return render(request, 'polls/detail.html', {'question': question})
+class DetailView(DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
-    return render(request, 'polls/detail.html', {'question': question})
+
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#
+#     return render(request, 'polls/results.html', {'question': question})
+class ResultsView(DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
+# -- Function-based View
 def vote(request, question_id):
+    logger.debug("vote().question_id: %s" % question_id)
     question = get_object_or_404(Question, pk=question_id)
 
     try:
@@ -30,9 +56,3 @@ def vote(request, question_id):
         selected_choice.save()
 
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
-
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-
-    return render(request, 'polls/results.html', {'question': question})
